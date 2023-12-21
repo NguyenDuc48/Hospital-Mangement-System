59b6d45ead9ff9cbac8cc68fc52939ce17552a28
Raisel <122186395+BuiDucHieuK66@users.noreply.github.com>
2023-12-21 09:04:21 +0700

    Now manager.js can run

--- routes/users/Manager/manager.js
+++ routes/users/Manager/manager.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const doctor = express.Router();
-const nurse = express.Router();
 
 const db = require('../../../utils/db');
 
@@ -18,93 +17,115 @@ doctor.get('/get_doctor', (req, res) => {
     });
 });
 
+// doctor.post('/add_patient', (req, res) => {
+
+//     const doctorData = {
+//         patient_id  : req.body.patient_id,
+//         full_name   : req.body.full_name,
+//         dob         : req.body.dob,
+//         gender      : req.body.gender,
+//         phone_number: req.body.phone_number,
+//         address     : req.body.address,
+//         email       : req.body.email
+//     }
+
+//     let find = `SELECT * FROM patient WHERE patient_id = "${patientData.patient_id}"`;
+
+//     db.query(find, (err1, result1) => {
+//         if(err1) console.log(err1);
+//         //console.log(result1[0]);
+
+//         if(result1[0] == undefined) {  
+//             let create = `INSERT INTO patient (
+//                     patient_id,
+//                     full_name,
+//                     dob,
+//                     gender,
+//                     phone_number,
+//                     address,
+//                     email)
+//                               VALUES ( "${patientData.patient_id}", 
+//                                        "${patientData.full_name}",
+//                                        "${patientData.dob}",
+//                                        "${patientData.gender}",
+//                                        "${patientData.phone_number}",
+//                                        "${patientData.address}",
+//                                        "${patientData.email}")`;
+//             db.query(create, (err2, result2) => {
+//                 if(err2) console.log(err2);
+//                 res.send("Created Database ooooooooooooohhhhhh");
+//             })
+//         }else {
+//             res.send("user already exist...");
+//         }
+//     });
+// });
+
 doctor.post('/add_doctor', (req, res) => {
     const doctorData = {
-        first_name  : req.body.first_name,
-        middle_name : req.body.middle_name,
-        last_name   : req.body.last_name,
-        gender      : req.body.gender,
-        address     : req.body.address,
-        email       : req.body.email,
-        phone_number: req.body.phone_number,
+        doctor_id   : req.body.doctor_id,
+        full_name   : req.body.full_name,
         dob         : req.body.dob,
+        gender      : req.body.gender,
+        phone_number: req.body.phone_number,
+        email       : req.body.email,
+        address     : req.body.address,
+        salary      : req.body.salary,
+        work_from   : req.body.work_from,
+        expertise   : req.body.expertise,
+        department  : req.body.department,
         password    : req.body.password
-    };
+    }
 
-    let find = `SELECT * FROM doctors WHERE email = "${doctorData.email}"`;
+    let find = `SELECT * FROM doctors WHERE doctor_id = "${doctorData.doctor_id}"`;
 
     db.query(find, (err1, result1) => {
-        if(err1) {
-            console.log(err1);
-            res.status(500).json({ error: 'Error checking for existing doctor' });
-        }
+        if(err1) console.log(err1);
+        console.log(result[0]);
 
-        if(result1.length === 0) {
-            bcrypt.hash(req.body.password, 10, (err, hash) => {
-                doctorData.password = hash;
+        if(result1[0] == undefined) {
+            let create = `INSERT INTO employees (employee_id, full_name, dob, gender, phone_number, email, address, salary, work_from)
+                              VALUES ("${doctorData.doctor_id}", 
+                                       "${doctorData.full_name}",
+                                       "${doctorData.dob}", 
+                                       "${doctorData.gender}", 
+                                       "${doctorData.phone_number}",
+                                       "${doctorData.email}",
+                                       "${doctorData.address}",
+                                       "${doctorData.salary}",
+                                       "${doctorData.work_from}")`;
 
-                let createEmployee = `INSERT INTO employees (
-                    first_name,
-                    middle_name,
-                    last_name, 
-                    gender,
-                    address,
-                    email, 
-                    phone_number,
-                    dob)
-                    VALUES ("${doctorData.first_name}", 
-                            "${doctorData.middle_name}", 
-                            "${doctorData.last_name}", 
-                            "${doctorData.gender}",
-                            "${doctorData.address}",
-                            "${doctorData.email}",
-                            "${doctorData.phone_number}",
-                            "${doctorData.dob}")`;
+            db.query(create, (err2, result2) => {
+                if(err2) console.log(err2);
 
-                db.query(createEmployee, (err2, result2) => {
-                    if(err2) {
-                        console.log(err2);
-                        res.status(500).json({ error: 'Error creating employee' });
-                    }
+                let create_account = `INSERT INTO credentials (username, password, id) 
+                                      VALUES ("${doctorData.full_name}",
+                                              "${doctorData.password}",
+                                              "${doctorData.doctor_id}")`;
 
-                    let employee_id = result2.insertId;
+                db.query(create_account, (err3, result3) => {
+                    if (err3) console.log(err3);
 
-                    let createDoctor = `INSERT INTO doctors (
-                        doctor_id,
-                        expertise,
-                        department)
-                        VALUES ("${employee_id}",
-                                "${req.body.expertise}",
-                                "${req.body.department}")`;
+                    let create_doctor = `INSERT INTO doctors (doctor_id, expertise, department)
+                                         VALUES ("${doctorData.doctor_id}",
+                                                  ${doctorData.expertise}",
+                                                  ${doctorData.department}")`
 
-                    let createCredentials = `INSERT INTO credentials (
-                        username,
-                        password,
-                        id)
-                        VALUES (CONCAT_WS(' ', "${doctorData.first_name}", "${doctorData.middle_name}", "${doctorData.last_name}"),
-                                "${doctorData.password}",
-                                "${employee_id}")`;
-
-                    db.query(createDoctor, createCredentials, (err3, result3) => {
-                        if(err3) {
-                            console.log(err3);
-                            res.status(500).json({ error: 'Error creating doctor' });
-                        }
-
-                        res.json({ success: true, message: 'Doctor created successfully' });
-                    });
+                    db.query(create_doctor, (err4, result4) => {
+                        if (err4) console.log(err4);
+                        res.send("Doctor created successfully");
+                    })
                 });
             });
         } else {
-            res.status(400).json({ error: 'Doctor already exists' });
+            res.send("Doctor already exists...");
         }
     });
 });
 
 doctor.put('/update_doctor', (req, res) => {
-    const doctor_id = req.body.doctor_id;
-
     const updatedData = {
+        doctor_id: req.body.doctor_id,
         expertise: req.body.expertise,
         department: req.body.department,
         email: req.body.email
@@ -112,9 +133,8 @@ doctor.put('/update_doctor', (req, res) => {
 
     let updateQuery = `UPDATE doctors
                        SET expertise = "${updatedData.expertise}",
-                           department = "${updatedData.department}",
-                           email = "${updatedData.email}"
-                       WHERE doctor_id = "${doctor_id}"`;
+                           department = "${updatedData.department}"
+                       WHERE doctor_id = "${updatedData.doctor_id}"`;
 
     db.query(updateQuery, (err, result) => {
         if (err) {
@@ -130,24 +150,24 @@ doctor.delete('/delete_doctor', (req, res) => {
     const doctor_id = req.body.doctor_id;
 
     let deleteDoctor = `DELETE FROM doctors 
-                         WHERE doctor_id = "${doctor_id}"`;
-
-    let deleteEmployee = `DELETE FROM employees 
-                          WHERE employee_id = "${doctor_id}"`;
+                        WHERE doctor_id = "${doctor_id}"`;
 
     db.query(deleteDoctor, (err, result) => {
         if (err) {
             console.log(err);
             res.status(500).json({ error: 'Error deleting doctor information' });
+        } 
+    });
+
+    let deleteEmployee = `DELETE FROM employees 
+                          WHERE employee_id = "${doctor_id}"`;
+
+    db.query(deleteEmployee, (err2, result2) => {
+        if (err2) {
+            console.log(err2);
+            res.status(500).json({ error: 'Error deleting associated employee information' });
         } else {
-            db.query(deleteEmployee, (err2, result2) => {
-                if (err2) {
-                    console.log(err2);
-                    res.status(500).json({ error: 'Error deleting associated employee information' });
-                } else {
-                    res.json({ success: true, message: 'Doctor information deleted successfully' });
-                }
-            });
+            res.json({ success: true, message: 'Doctor information deleted successfully' });
         }
     });
 });
