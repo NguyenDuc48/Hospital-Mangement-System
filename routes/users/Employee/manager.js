a3d6c1284701445c0784fe871c720bed9465c3e8
Raisel <122186395+BuiDucHieuK66@users.noreply.github.com>
2023-12-22 04:37:36 +0700

    Update manager.js

--- routes/users/Employee/manager.js
+++ routes/users/Employee/manager.js
@@ -7,18 +7,10 @@ const db = require('../../../utils/db');
 
 process.env.SECRET_KEY = 'Arijit';
 
+//-------------------------------------DOCTOR-----------------------------------
+
 employee.get('/get_doctor', (req, res) => {
     const sql = "SELECT * FROM doctors JOIN employees ON doctors.doctor_id = employees.employee_id";
-
-    db.query(sql, (err, result) => {
-        if(err) console.log(err);
-        console.log(result)
-        res.json(result);
-    });
-});
-
-employee.get('/get_nurse', (req, res) => {
-    const sql = "SELECT * FROM nurses JOIN employees ON nurses.nurse_id = employees.employee_id";
 
     db.query(sql, (err, result) => {
         if(err) console.log(err);
@@ -50,7 +42,7 @@ employee.post('/add_doctor', (req, res) => {
         // console.log(result1[0]);
 
         if(result1[0] == undefined) {
-            let create = `INSERT INTO employees (employee_id, full_name, dob, gender, phone_number, email, address, salary, work_from)
+            let create = `INSERT INTO employees (employee_id, full_name, dob, gender, phone_number, email, address, salary, work_from, status)
                               VALUES ("${doctorData.doctor_id}", 
                                        "${doctorData.full_name}",
                                        "${doctorData.dob}", 
@@ -59,7 +51,8 @@ employee.post('/add_doctor', (req, res) => {
                                        "${doctorData.email}",
                                        "${doctorData.address}",
                                        "${doctorData.salary}",
-                                       "${doctorData.work_from}")`;
+                                       "${doctorData.work_from}",
+                                       "Having job")`;
 
             db.query(create, (err2, result2) => {
                 if(err2) console.log(err2);
@@ -111,18 +104,126 @@ employee.put('/update_doctor', (req, res) => {
     });
 });
 
+employee.route('/delete_doctor')
+    .delete((req, res) => {
+        const doctor_id = req.body.doctor_id;
+
+        let delete_account = `DELETE FROM credentials WHERE id = "${doctor_id}"`;
+
+        db.query(delete_account, (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(500).json({ error: 'Error deleting account' });
+            } else {
+                console.log("OK");
+                res.json({ success: true, message: 'Account deleted successfully' });
+            }
+        });
+    })
+    .put((req, res) => {
+        const doctor_id = req.body.doctor_id;
+
+        let update_status = `UPDATE employees
+                             SET status = "Retired"
+                             WHERE employee_id = "${doctor_id}"`;
+
+        db.query(update_status, (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(500).json({ error: 'Error updating status' });
+            } else {
+                console.log("OK");
+                res.json({ success: true, message: 'Status updated successfully' });
+            }
+        });
+    });
+
+//--------------------------------------NURSE-------------------------------------
+
+employee.get('/get_nurse', (req, res) => {
+    const sql = "SELECT * FROM nurses JOIN employees ON nurses.nurse_id = employees.employee_id";
+
+    db.query(sql, (err, result) => {
+        if(err) console.log(err);
+        console.log(result)
+        res.json(result);
+    });
+});
+    
+employee.post('/add_nurse', (req, res) => {
+    const nurseData = {
+        nurse_id   : req.body.nurse_id,
+        full_name   : req.body.full_name,
+        dob         : req.body.dob,
+        gender      : req.body.gender,
+        phone_number : req.body.phone_number,
+        email       : req.body.email,
+        address     : req.body.address,
+        salary      : req.body.salary,
+        work_from   : req.body.work_from,
+        department  : req.body.department,
+        password    : req.body.password
+    }
+    
+    let find = `SELECT * FROM nurses WHERE nurse_id = "${nurseData.nurse_id}"`;
+    
+    db.query(find, (err1, result1) => {
+        if(err1) console.log(err1);
+        // console.log(result1[0]);
+    
+        if(result1[0] == undefined) {
+            let create = `INSERT INTO employees (employee_id, full_name, dob, gender, phone_number, email, address, salary, work_from, status)
+                              VALUES ("${nurseData.nurse_id}", 
+                                       "${nurseData.full_name}",
+                                       "${nurseData.dob}", 
+                                       "${nurseData.gender}", 
+                                       "${nurseData.phone_number}",
+                                       "${nurseData.email}",
+                                       "${nurseData.address}",
+                                       "${nurseData.salary}",
+                                       "${nurseData.work_from}",
+                                       "Having job")`;
+    
+            db.query(create, (err2, result2) => {
+                if(err2) console.log(err2);
+            });
+    
+            let create_account = `INSERT INTO credentials (username, password, id) 
+                                      VALUES ("${nurseData.full_name}",
+                                              "${nurseData.password}",
+                                              "${nurseData.nurse_id}")`;
+    
+            db.query(create_account, (err3, result3) => {
+                if (err3) console.log(err3);
+            });
+    
+            let create_nurse = `INSERT INTO nurses (nurse_id, department, shift)
+                                     VALUES ("${nurseData.nurse_id}",
+                                             "${nurseData.department}",
+                                             "Trống")`
+    
+            db.query(create_nurse, (err4, result4) => {
+                if (err4) console.log(err4);
+                res.send("nurse created successfully");
+            })
+        } else {
+            res.send("nurse already exists...");
+        }
+    });
+});
+    
 employee.put('/update_nurse', (req, res) => {
     const updatedData = {
         nurse_id: req.body.nurse_id,
         department: req.body.department,
         shift: req.body.shift
     };
-
+    
     let updateQuery = `UPDATE nurses
                        SET department = "${updatedData.department}",
                            shift = "${updatedData.shift}"
                        WHERE nurse_id = "${updatedData.nurse_id}"`;
-
+    
     db.query(updateQuery, (err, result) => {
         if (err) {
             console.log(err);
@@ -132,42 +233,39 @@ employee.put('/update_nurse', (req, res) => {
         }
     });
 });
-
-employee.delete('/delete_doctor', (req, res) => {
-    const doctor_id = req.body.doctor_id;
-
-    let deleteDoctor = `DELETE FROM doctors 
-                        WHERE doctors.doctor_id = "${doctor_id}"`;
-
-    db.query(deleteDoctor, (err, result) => {
-        if (err) {
-            console.log(err);
-            res.status(500).json({ error: 'Error deleting doctor information' });
-        } 
-        console.log("OK")
-    });
-
-    let delete_account = `DELETE FROM credentials WHERE id = "${doctor_id}"`
-
-    db.query(delete_account, (err3, result3) => {
-        if (err3) {
-            console.log(err);
-            res.status(500).json({ error: 'Error deleting account' });
-        }
+    
+employee.route('/delete_nurse')
+    .delete((req, res) => {
+        const nurse_id = req.body.nurse_id;
+    
+        let delete_account = `DELETE FROM credentials WHERE id = "${nurse_id}"`;
+    
+        db.query(delete_account, (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(500).json({ error: 'Error deleting account' });
+            } else {
+                console.log("OK");
+                res.json({ success: true, message: 'Account deleted successfully' });
+            }
+        });
     })
-
-    let deleteEmployee = `DELETE FROM employees 
-                          WHERE employees.employee_id = "${doctor_id}"`;
-
-    db.query(deleteEmployee, (err2, result2) => {
-        if (err2) {
-            console.log(err2);
-            res.status(500).json({ error: 'Error deleting associated employee information' });
-        } else {
-            console.log("OK")
-            res.json({ success: true, message: 'Doctor information deleted successfully' });
-        }
+    .put((req, res) => {
+        const nurse_id = req.body.nurse_id;
+    
+        let update_status = `UPDATE employees
+                             SET status = "Retired"
+                             WHERE employee_id = "${nurse_id}"`;
+    
+        db.query(update_status, (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(500).json({ error: 'Error updating status' });
+            } else {
+                console.log("OK");
+                res.json({ success: true, message: 'Status updated successfully' });
+            }
+        });
     });
-});
-
+    
 module.exports = employee;
