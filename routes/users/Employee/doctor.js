c94b0da03b7518f3bf60767aa0ea18e6e037b7f2
Raisel <122186395+BuiDucHieuK66@users.noreply.github.com>
2023-12-23 10:34:37 +0700

    Update doctor.js

--- routes/users/Employee/doctor.js
+++ routes/users/Employee/doctor.js
@@ -7,49 +7,12 @@ const db = require('../../../utils/db');
 
 process.env.SECRET_KEY = 'Arijit';
 
-doctor.post('/register', (req, res) => {
+// Personal profile management
+//  View the patient's medical history
 
-    const doctorData = {
-        first_name      : req.body.first_name,
-        last_name       : req.body.last_name,
-        address         : req.body.address,
-        email           : req.body.email,
-        salary          : req.body.salary,
-        specialisation  : req.body.specialisation,
-        shift_time      : req.body.shift_time,
-        password        : req.body.password
-    }
+//  Update reports for patients
 
-    let find = `SELECT * FROM doctors WHERE email = "${doctorData.email}"`;
 
-    db.query(find, (err1, result1) => {
-        if(err1) console.log(err1);
-        console.log(result1[0]);
-
-        if(result1[0] == undefined) {
-            bcrypt.hash(req.body.password, 10, (err, hash) => {
-                doctorData.password = hash;
-                
-                let create = `INSERT INTO doctors (first_name, last_name, address, email, salary, specialisation, shift_time, password)
-                              VALUES ( "${doctorData.first_name}", 
-                                       "${doctorData.last_name}", 
-                                       "${doctorData.address}", 
-                                       "${doctorData.email}",
-                                       "${doctorData.salary}",
-                                       "${doctorData.specialisation}",
-                                       "${doctorData.shift_time}",
-                                       "${doctorData.password}")`;
-
-                db.query(create, (err2, result2) => {
-                    if(err2) console.log(err2);
-                    res.send("Created Database ooooooooooooohhhhhh");
-                })
-            });
-        }else {
-            res.send("doctor already exist...");
-        }
-    });
-});
 
 doctor.post('/login', (req, res) => {
     let find = `SELECT password, doctor_id FROM doctors WHERE email = "${req.body.email}"`;
@@ -71,14 +34,16 @@ doctor.post('/login', (req, res) => {
     });
 });
 
+//  View patient appointments
 doctor.get('/patient', (req,res) => {
     let doctor_id = jwt.verify(req.headers['authorization'], process.env.SECRET_KEY);
     
     const sql = `SELECT 
                     p.patient_id,
                     p.first_name,
+                    p.middle_name,
                     p.last_name
-                FROM assign_doctor ad
+                FROM doctor 
                     JOIN patient p ON p.patient_id = ad.patient_id
                     JOIN doctors d ON d.doctor_id = ad.doctor_id
                 WHERE ad.doctor_id = ${doctor_id}
@@ -95,11 +60,11 @@ doctor.get('/patient', (req,res) => {
 doctor.get('/profile', (req, res) => {
     let doctor_id = jwt.verify(req.headers['authorization'], process.env.SECRET_KEY);
     
-    let user = `SELECT * FROM doctors WHERE doctor_id = ${doctor_id}`;
+    let user = `SELECT * FROM doctors JOIN employees ON doctors.doctor_id = employees.employee_id 
+                WHERE doctor_id = "${doctor_id.doctor_id}"`;
     db.query(user, (err, result) => {
         if (err) console.log(err);
         res.send(result);
-
     });
 });
 
@@ -116,6 +81,29 @@ doctor.post('/delete', (req, res) => {
             })
         }
     })
+});
+
+doctor.put('/update_doctor', (req, res) => {
+    // let employee_id = req.body.employee_id;
+    const updatedData = {
+        expertise: req.body.expertise,
+        department: req.body.department
+    };
+
+    let updateQuery = `UPDATE doctor
+                       SET expertise = "${updatedData.expertise}",
+                           department = "${updatedData.department}",
+                           email = "${updatedData.email}",
+                       WHERE doctor_id = "${doctor_id}"`;
+
+    db.query(updateQuery, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ error: 'Error updating employee information' });
+        } else {
+            res.json({ success: true, message: 'Employee information updated successfully' });
+        }
+    });
 });
 
 doctor.post('/update_sal', (req, res) => {
