54498504b60f353b90f3358134dccbfc698b5317
Raisel <122186395+BuiDucHieuK66@users.noreply.github.com>
2023-12-25 13:44:33 +0700

    Update patient.js

--- routes/users/Patient/patient.js
+++ routes/users/Patient/patient.js
@@ -20,13 +20,13 @@ patient.post('/add_patient', (req, res) => {
         password    : req.body.password
     }
 
-    let find = `SELECT * FROM patient WHERE patient_id = "${patientData.patient_id}"`;
+    let find = `SELECT patient_id FROM patient WHERE patient_id = "${patientData.patient_id}"`;
 
     db.query(find, (err1, result1) => {
         if(err1) console.log(err1);
         //console.log(result1[0]);
 
-        if(result1[0] == undefined) {  
+        if (result1[0] == undefined) {  
             let create = `INSERT INTO patient (
                     patient_id,
                     full_name,
@@ -34,14 +34,16 @@ patient.post('/add_patient', (req, res) => {
                     gender,
                     phone_number,
                     address,
-                    email)
+                    email,
+                    health_insurance_percent)
                               VALUES ( "${patientData.patient_id}", 
                                        "${patientData.full_name}",
                                        "${patientData.dob}",
                                        "${patientData.gender}",
                                        "${patientData.phone_number}",
                                        "${patientData.address}",
-                                       "${patientData.email}")`;
+                                       "${patientData.email}",
+                                       "0")`;
             db.query(create, (err2, result2) => {
                 if(err2) console.log(err2);
                 
