69756e6916781490dc6fd20da3cce9a988bc44fd
Raisel <122186395+BuiDucHieuK66@users.noreply.github.com>
2023-12-27 23:41:08 +0700

    Add files via upload

---
+++ routes/login.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const login = express.Router();
+
+const db = require('../../utils/db');
+
+process.env.SECRET_KEY = 'Arijit';
+
+login.post('/', (req, res) => {
+    const { username, password } = req.body;
+    const find = `SELECT password, id FROM credentials WHERE username = "${username}"`;
+    console.log("reqbody:" , req.body)
+    db.query(find, (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Internal Server Error' });
+        return;
+      }
+  
+      if (result.length > 0) {
+        const storedPassword = result[0].password;
+        console.log("store pass:", storedPassword)
+        
+        bcrypt.compare(password, storedPassword, (bcryptErr, bcryptResult) => {
+          if (bcryptErr) {
+            console.error(bcryptErr);
+            res.status(500).json({ message: 'Internal Server Error' });
+            return;
+          }
+          console.log("bcryptResult:", bcryptResult)
+        //  password trong database dạng hash thì sẽ dùng so sánh này
+          // if (bcryptResult){ 
+          if (password === result[0].password) {
+            let userId = result[0].id.toString();
+            console.log("Userid:" , userId)
+            const token = jwt.sign({ userId }, process.env.SECRET_KEY, { expiresIn: '1h' });
+            if (userId.substring(0,2)=== "QL") {
+                res.status(200).json({ message: 'Login manager successful', token });
+            }
+            else if (userId.substring(0,2)=== "BS") {
+                res.status(201).json({ message: 'Login doctor successful', token });
+            }
+            else if (userId.substring(0,2)=== "BN") {
+                res.status(202).json({ message: 'Login patient successful', token });
+            }
+            else {
+                res.status(203).json({ message: 'Login nurse successful', token });
+            } 
+          } else {
+            res.status(401).json({ message: 'Incorrect password' });
+          }
+        });
+      } else {
+        res.status(401).json({ message: 'Username not found' });
+      }
+    });
+  });
+
+module.exports = login;
