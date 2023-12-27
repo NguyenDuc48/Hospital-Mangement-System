b0bc69ceef4be07c7be63bbbb0fe5acb037ad317
Raisel <122186395+BuiDucHieuK66@users.noreply.github.com>
2023-12-27 23:38:47 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,35 +2,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const Users = require('./routes/users');
 const Patient = require('./routes/users/Patient/patient');
-const Employee = require('./routes/users/Employee/employee');
 const Doctor = require('./routes/users/Employee/doctor');
-const Admin = require('./routes/users/Administrator/admin');
-const superAdmin = require('./routes/users/Administrator/superAdmin');
 const Manager = require('./routes/users/Employee/manager');
-const api = require('./routes/api/api');
-
+const Nurse = require('./routes/users/Employee/nurse');
+const Login = require('./routes/users/Login/login');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/users', Users);
 app.use('/patient', Patient);
-app.use('/employee', Employee);
 app.use('/doctor' , Doctor);
-app.use('/admin', Admin);
-app.use('/super', superAdmin);
 app.use('/manager', Manager);
-app.use('/api', api);
+app.use('/nurse', Nurse);
+app.use('/manager', Manager);
+app.use('/login', Login);
 
 app.use(express.static(path.join(__dirname + '/client/build')));
 
 app.use('/*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
