6939664b7e5b84fdc8061b7691f368c402b2c779
Raisel <122186395+BuiDucHieuK66@users.noreply.github.com>
2023-12-30 11:56:31 +0700

    Update doctor.js

--- routes/users/Employee/doctor.js
+++ routes/users/Employee/doctor.js
@@ -30,6 +30,7 @@ doctor.get('/waiting_list', (req,res) => {
     });
 })
 
+// Personal profile management
 doctor.get('/profile', (req, res) => {
     let doctor_id = jwt.verify(req.headers['authorization'].replace('Bearer ', ''), process.env.SECRET_KEY)
     
@@ -41,23 +42,7 @@ doctor.get('/profile', (req, res) => {
     });
 });
 
-doctor.post('/delete', (req, res) => {
-    const find = `SELECT * FROM doctors WHERE doctor_id = ${req.body.doctor_id}`;
-    let del =  `DELETE FROM doctors WHERE doctor_id = ${req.body.doctor_id}`
-
-    db.query(find, (err1, result1) => {
-        if(err1) console.log(err1);
-
-        if(result1[0] != undefined) {
-            db.query(del, (err2, result2) => {
-                res.send('DELETED');
-            })
-        }
-    })
-});
-
 doctor.put('/update_me', (req, res) => {
-    // let employee_id = req.body.employee_id;
     let employee_id = jwt.verify(req.headers['authorization'].replace('Bearer ', ''), process.env.SECRET_KEY)
     const updatedData = {
         full_name : req.body.full_name,
@@ -86,5 +71,59 @@ doctor.put('/update_me', (req, res) => {
         }
     });
 });
+
+doctor.put('/call_patient', (req, res) => {
+    let wait_id = jwt.verify(req.headers['authorization'].replace('Bearer ', ''), process.env.SECRET_KEY)
+    let call = `UPDATE wait_list
+                SET status = "in progress"
+                WHERE wait_id = "${wait_id.wait_id}"`
+
+    db.query(call, (err, result) => {
+        if (err) console.log(err);
+        res.send("Updated successfully");
+    });
+})
+
+//  Update reports for patients
+doctor.put('/update_report', (req, res) => {
+    const report = {
+        diagnostic  : req.body.diagnostic,
+        conclusion  : req.body.conclusion,
+        note        : req.body.note,
+        report_id   : req.body.report_id
+    }
+
+    let update = `UPDATE medical_reports
+                  SET diagnostic = "${report.diagnostic}",
+                      conclusion = "${report.conclusion}",
+                      note = "${report.note}"
+                  WHERE report_id = "${report.report_id}"`
+
+    db.query(update, (err, result) => {
+        if (err) console.log(err);
+        res.send("Updated successfully");
+    });
+})
+
+//Nếu có tạo token cho từng cái medic_report thì dùng cái này
+// doctor.put('/update_report', (req, res) => {
+//     let report_id = jwt.verify(req.headers['authorization'].replace('Bearer ', ''), process.env.SECRET_KEY)
+//     const report = {
+//         diagnostic  : req.body.diagnostic,
+//         conclusion  : req.body.conclusion,
+//         note        : req.body.note,
+//     }
+
+//     let update = `UPDATE medical_reports
+//                   SET diagnostic = "${report.diagnostic}",
+//                       conclusion = "${report.conclusion}",
+//                       note = "${report.note}"
+//                   WHERE report_id = "${report_id.report_id}"`
+
+//     db.query(update, (err, result) => {
+//         if (err) console.log(err);
+//         res.send("Updated successfully");
+//     });
+// })
 
 module.exports = doctor;
