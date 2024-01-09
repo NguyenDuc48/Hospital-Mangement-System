3fa827e7b4a8ccb446cd4e081ccc02cc9995aeff
Raisel <122186395+BuiDucHieuK66@users.noreply.github.com>
2024-01-09 19:04:27 +0700

    Update doctor.js

--- routes/users/Employee/doctor.js
+++ routes/users/Employee/doctor.js
@@ -10,24 +10,38 @@ process.env.SECRET_KEY = 'Arijit';
 doctor.get('/waiting_list', (req,res) => {
     let doctor_id = jwt.verify(req.headers['authorization'].replace('Bearer ', ''), process.env.SECRET_KEY)
     
-    const get_priority = `SELECT DISTINCT wl.wait_id, p.full_name, wl.priority
-                          FROM wait_list wl JOIN medical_reports mr ON wl.patient_id = mr.patient_id
-                                            JOIN patient p ON p.patient_id = wl.patient_id 
-                          WHERE mr.doctor_id = "${doctor_id.userId}" AND wl.priority = "yes" AND wl.status = "waiting" AND wl.status = "in progress"`
+    const find_department = `SELECT department FROM doctors WHERE doctor_id = "${doctor_id.userId}"`
 
-    db.query(get_priority, (err, result) => {
-        if (err) console.log(err);
+    db.query(find_department, (err0, result0) => {
+        if (err0) console.log(err0)
+        console.log(result0)
+        const department = result0[0].department
 
-        const get_non_priority = `SELECT DISTINCT wl.wait_id, p.full_name, wl.priority
-                                  FROM wait_list wl JOIN medical_reports mr ON wl.patient_id = mr.patient_id
-                                                    JOIN patient p ON p.patient_id = wl.patient_id 
-                                  WHERE mr.doctor_id = "${doctor_id.userId}" AND wl.priority = "no" AND wl.status = "waiting" AND wl.status = "in progress"`
+        const get_in_progress = `SELECT p.full_name, wl.priority, wl.doctor_id
+                             FROM wait_list wl JOIN patient p ON p.patient_id = wl.patient_id 
+                             WHERE wl.doctor_id = "${doctor_id.userId}"`
 
-        db.query(get_non_priority, (err1, result1) => {
-            if (err1) console.log(err1);
-            res.send([result, result1]);
-        })
-    });
+        db.query(get_in_progress, (err, result) => {
+            if (err) console.log(err);
+
+            const get_priority = `SELECT DISTINCT p.full_name, wl.priority, wl.doctor_id
+                                FROM wait_list wl JOIN patient p ON p.patient_id = wl.patient_id 
+                                WHERE wl.priority = "yes" AND wl.status = "waiting" AND department_id = "${department}"`
+
+            db.query(get_priority, (err1, result1) => {
+                if (err1) console.log(err1);
+
+                const get_non_priority = `SELECT DISTINCT p.full_name, wl.priority, wl.doctor_id
+                                        FROM wait_list wl JOIN patient p ON p.patient_id = wl.patient_id 
+                                        WHERE wl.priority = "no" AND wl.status = "waiting" AND department_id = "${department}"`
+
+                db.query(get_non_priority, (err2, result2) => {
+                    if (err2) console.log(err2)
+                    res.send([result, result1, result2]);
+                })
+            })
+        });
+    })
 })
 
 // Personal profile management
@@ -73,7 +87,7 @@ doctor.put('/update_me', (req, res) => {
 });
 
 doctor.put('/call_patient', (req, res) => {
-    let wait_id = jwt.verify(req.headers['authorization'].replace('Bearer ', ''), process.env.SECRET_KEY)
+    let wait_id = req.body.wait_id
     let call = `UPDATE wait_list
                 SET status = "in progress"
                 WHERE wait_id = "${wait_id.wait_id}"`
@@ -85,10 +99,10 @@ doctor.put('/call_patient', (req, res) => {
 })
 
 doctor.put('/completed_examination_patient', (req, res) => {
-    let wait_id = jwt.verify(req.headers['authorization'].replace('Bearer ', ''), process.env.SECRET_KEY)
+    let wait_id = req.body.wait_id
     let call = `UPDATE wait_list
                 SET status = "paying"
-                WHERE wait_id = "${wait_id.wait_id}"`
+                WHERE wait_id = "${wait_id}"`
 
     db.query(call, (err, result) => {
         if (err) console.log(err);
@@ -117,9 +131,46 @@ doctor.put('/update_report', (req, res) => {
     });
 })
 
-doctor.post('/create_bill', (req, res) => {
+doctor.post('/create', (req, res) => {
     let create_bill = `INSERT INTO total_bills (service_bill_id, medicine_bill_id, equipment_bill_id, total_bill_raw) 
                        VALUES (NULL, NULL, NULL, NULL)`;
+
+    db.query(create_bill, (err2, result2) => {
+        if (err2) console.log(err2);
+        res.send(result2);
+
+        const time = new Date();
+        const current_time = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
+        const current_date = `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`
+
+        let create_medic_report = `INSERT INTO medical_reports 
+                                       (report_id, 
+                                        patient_id, 
+                                        doctor_id, 
+                                        diagnostic, 
+                                        conclusion, 
+                                        note, 
+                                        booking_time, 
+                                        appointment_date, 
+                                        bill_id, 
+                                        money_need_to_pay)  
+                                   VALUES (NULL, 
+                                           "${id.patient_id}", 
+                                           "${id.doctor_id}", 
+                                           NULL, 
+                                           NULL, 
+                                           NULL, 
+                                           "${current_time}", 
+                                           "${current_date}", 
+                                           (SELECT total_bill_id 
+                                            FROM total_bills 
+                                            ORDER BY total_bill_id DESC LIMIT 1), 
+                                           0)`
+                        
+        db.query(create_medic_report, (err3, result3) => {
+            if (err3) console.log(err3);
+        })
+    });
 
     db.query(create_bill, (err, result) => {
         if (err) console.log(err);
